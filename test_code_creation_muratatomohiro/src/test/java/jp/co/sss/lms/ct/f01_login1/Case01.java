88ec332ed85a96dfa1cf41f7ed56ff6d12ad43a9
t-murata <th.murata@syslg.co.jp>
2025-11-20 13:41:31 +0900

    add:Case01.java ログイン画面への遷移テストコード追加
    WebElementを利用してタイトルの値とログインボタンの値を取得し、それぞれ期待値通りかどうか確認するテストを作成。
    また、遷移先のURLが正しいかも確認を行う。
    期待値は以下の通り
    タイトル：ログイン | LMS
    ログインボタン：ログイン
    URL：http://localhost:8080/lms/

--- test_code_creation_muratatomohiro/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_muratatomohiro/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,7 +37,15 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms/");
+
+		getEvidence(new Object() {
+		}, "ケース01_No.01");
+
+		assertEquals(webDriver.getTitle(), "ログイン | LMS");
+		assertEquals(webDriver.findElement(By.className("btn")).getAttribute("value"), "ログイン");
+		assertEquals(webDriver.getCurrentUrl(), "http://localhost:8080/lms/");
 	}
 
 }
